4e1aac752529fa5489d823ed0e0f9d2f50251000
flukelaster <leviathanria@gmail.com>
2025-01-14 14:01:35 +0700

    up to date

---
+++ Desktop/Figma-Token/build/ios-swift/StyleDictionary+Enum.swift
@@ -0,0 +1,97 @@
+
+//
+// StyleDictionary+Enum.swift
+//
+
+// Do not edit directly, this file was auto-generated.
+
+
+import UIKit
+
+public enum StyleDictionaryEnum {
+    public static let bgPrimaryBgSection = UIColor(red: 0.945, green: 0.973, blue: 1.000, alpha: 1)
+    public static let bgPrimaryPageBg = UIColor(red: 0.957, green: 0.961, blue: 0.973, alpha: 1)
+    public static let bgPrimaryWhiteBg = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let borderRadiusLg = 24
+    public static let borderRadiusMd = 16
+    public static let borderRadiusSm = 8
+    public static let borderRadiusXl = 32
+    public static let borderRadiusXs = 4
+    public static let dimension2xl = CGFloat(32.00) * 4
+    public static let dimension3xl = CGFloat(32.00) * 4 * 2
+    public static let dimension4xl = CGFloat(32.00) * 4 * 4
+    public static let dimensionLg = CGFloat(32.00)
+    public static let dimensionMd = CGFloat(16.00)
+    public static let dimensionSm = CGFloat(8.00)
+    public static let dimensionXl = CGFloat(64.00)
+    public static let dimensionXs = CGFloat(4.00)
+    public static let fontFamilyDbHeavent = DB Heavent
+    public static let fontSizesBody = 18
+    public static let fontSizesH1 = 18 * 1.125 * 1.125 * 1.125 * 1.125 * 1.125
+    public static let fontSizesH2 = 18 * 1.125 * 1.125 * 1.125 * 1.125
+    public static let fontSizesH3 = 18 * 1.125 * 1.125 * 1.125
+    public static let fontSizesH4 = 18 * 1.125 * 1.125
+    public static let fontSizesH5 = 18 * 1.125
+    public static let fontSizesRemark = 18 - 4
+    public static let fontWeightBold = Bold
+    public static let fontWeightMedium = Medium
+    public static let fontWeightRegular = Regular
+    public static let lineHeightsStandard = 100%
+    public static let opacity10 = 10%
+    public static let opacity25 = 25%
+    public static let opacity50 = 50%
+    public static let opacity75 = 75%
+    public static let opacity90 = 90%
+    public static let primitivesBlueBlue100 = UIColor(red: 0.945, green: 0.973, blue: 1.000, alpha: 1)
+    public static let primitivesBlueBlue200 = UIColor(red: 0.792, green: 0.890, blue: 1.000, alpha: 1)
+    public static let primitivesBlueBlue300 = UIColor(red: 0.290, green: 0.533, blue: 1.000, alpha: 1)
+    public static let primitivesBlueBlue400 = UIColor(red: 0.165, green: 0.443, blue: 1.000, alpha: 1)
+    public static let primitivesBlueBlue500 = UIColor(red: 0.004, green: 0.353, blue: 1.000, alpha: 1)
+    public static let primitivesBlueBlue700 = UIColor(red: 0.004, green: 0.286, blue: 0.800, alpha: 1)
+    public static let primitivesBrandingBrandCi = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let primitivesGreenGreen100 = UIColor(red: 0.973, green: 1.000, blue: 0.863, alpha: 1)
+    public static let primitivesGreenGreen200 = UIColor(red: 0.902, green: 1.000, blue: 0.604, alpha: 1)
+    public static let primitivesGreenGreen300 = UIColor(red: 0.498, green: 0.831, blue: 0.239, alpha: 1)
+    public static let primitivesGreenGreen400 = UIColor(red: 0.400, green: 0.757, blue: 0.180, alpha: 1)
+    public static let primitivesGreenGreen500 = UIColor(red: 0.298, green: 0.686, blue: 0.118, alpha: 1)
+    public static let primitivesGreenGreen700 = UIColor(red: 0.231, green: 0.529, blue: 0.090, alpha: 1)
+    public static let primitivesNeutralDark100 = UIColor(red: 0.957, green: 0.961, blue: 0.973, alpha: 1)
+    public static let primitivesNeutralDark200 = UIColor(red: 0.875, green: 0.875, blue: 0.875, alpha: 1)
+    public static let primitivesNeutralDark300 = UIColor(red: 0.812, green: 0.812, blue: 0.812, alpha: 1)
+    public static let primitivesNeutralDark400 = UIColor(red: 0.545, green: 0.545, blue: 0.545, alpha: 1)
+    public static let primitivesNeutralDark900 = UIColor(red: 0.118, green: 0.118, blue: 0.118, alpha: 1)
+    public static let primitivesNeutralWhite = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let primitivesPurplePurple100 = UIColor(red: 0.980, green: 0.957, blue: 1.000, alpha: 1)
+    public static let primitivesPurplePurple200 = UIColor(red: 0.910, green: 0.800, blue: 1.000, alpha: 1)
+    public static let primitivesPurplePurple300 = UIColor(red: 0.541, green: 0.400, blue: 1.000, alpha: 1)
+    public static let primitivesPurplePurple400 = UIColor(red: 0.451, green: 0.302, blue: 1.000, alpha: 1)
+    public static let primitivesPurplePurple500 = UIColor(red: 0.361, green: 0.200, blue: 1.000, alpha: 1)
+    public static let primitivesPurplePurple700 = UIColor(red: 0.290, green: 0.161, blue: 0.800, alpha: 1)
+    public static let primitivesRedRed100 = UIColor(red: 1.000, green: 0.961, blue: 0.961, alpha: 1)
+    public static let primitivesRedRed200 = UIColor(red: 0.996, green: 0.855, blue: 0.855, alpha: 1)
+    public static let primitivesRedRed300 = UIColor(red: 0.957, green: 0.463, blue: 0.463, alpha: 1)
+    public static let primitivesRedRed400 = UIColor(red: 0.941, green: 0.361, blue: 0.361, alpha: 1)
+    public static let primitivesRedRed500 = UIColor(red: 0.922, green: 0.267, blue: 0.267, alpha: 1)
+    public static let primitivesRedRed700 = UIColor(red: 0.765, green: 0.212, blue: 0.212, alpha: 1)
+    public static let primitivesYellowYellow100 = UIColor(red: 1.000, green: 0.976, blue: 0.890, alpha: 1)
+    public static let primitivesYellowYellow200 = UIColor(red: 1.000, green: 0.925, blue: 0.631, alpha: 1)
+    public static let primitivesYellowYellow300 = UIColor(red: 0.984, green: 0.816, blue: 0.290, alpha: 1)
+    public static let primitivesYellowYellow400 = UIColor(red: 0.980, green: 0.773, blue: 0.231, alpha: 1)
+    public static let primitivesYellowYellow500 = UIColor(red: 0.976, green: 0.796, blue: 0.173, alpha: 1)
+    public static let primitivesYellowYellow700 = UIColor(red: 0.788, green: 0.624, blue: 0.141, alpha: 1)
+    public static let spacing2xl = 40
+    public static let spacing3xl = 48
+    public static let spacingLg = 24
+    public static let spacingMd = 16
+    public static let spacingSm = 8
+    public static let spacingXl = 32
+    public static let spacingXs = 4
+    public static let textPrimaryBlue500 = UIColor(red: 0.000, green: 0.125, blue: 0.357, alpha: 1)
+    public static let textPrimaryContent900 = UIColor(red: 0.118, green: 0.118, blue: 0.118, alpha: 1)
+    public static let textPrimaryError500 = UIColor(red: 0.922, green: 0.267, blue: 0.267, alpha: 1)
+    public static let textPrimarySuccess500 = UIColor(red: 0.298, green: 0.686, blue: 0.118, alpha: 1)
+    public static let textPrimaryWarning500 = UIColor(red: 0.976, green: 0.796, blue: 0.173, alpha: 1)
+    public static let textSecondaryRamark300 = UIColor(red: 0.812, green: 0.812, blue: 0.812, alpha: 1)
+    public static let textSecondarySubcontent400 = UIColor(red: 0.545, green: 0.545, blue: 0.545, alpha: 1)
+    public static let textSecondaryWhite = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+}
